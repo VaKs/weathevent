3043aa96070c54f86b0972ee87e11e3cda720bf3
agpodpod <agpodpod@inf.upv.es>
2018-04-22 22:41:47 +0200

    Changed server

--- app/src/main/java/Fragment/FavouriteEventsFragment.java
+++ app/src/main/java/Fragment/FavouriteEventsFragment.java
@@ -31,24 +31,4 @@ public class FavouriteEventsFragment extends Fragment implements FragmentsInterf
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         return inflater.inflate(R.layout.fragment_favourite, container, false);
     }
-
-    @Override
-    public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
-        WeatheventActivity weatheventActivity = new WeatheventActivity();
-        Search searchEvent = new Search();
-        searchEvent.setLocationLatitude("51.7532339");
-        searchEvent.setLocationLongitude("0.4596447999999782");
-        searchEvent.setLocationWithin("5km");
-        eventsList = weatheventActivity.eventbriteSearch(searchEvent);
-        List<String> venues = new ArrayList<>();
-        ArrayList<Event> events = eventsList.getEvents();
-        for(int i=0; i < eventsList.size(); i++){
-            venues.add(events.get(i).getVenueId());
-        }
-        for (int j=0; j<venues.size();j++){
-            venue= weatheventActivity.eventbritegetVenue(venues.get(j));
-            String latitude = venue.getLatitude();
-            String longitude = venue.getLongitude();
-        }
-    }
 }
